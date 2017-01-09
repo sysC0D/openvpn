593fabbc41def1d04abf1f21606530bf37b6213b
sysc0d <codmonitoring75@gmail.com>
2017-01-09 18:13:46 +0100

    Add rules iptables after restart Docker

--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -3,6 +3,8 @@
 # Generate conf OpenVPN
 if [ ! -f /etc/openvpn/clients/*.tar ]; then
 	/var/tools/genere_user_ovpn.sh
+else
+	iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
 fi
 
 # Start Supervisord
